9a8b62e8359a125411bf15d07df581dd825d87cc
TermoLizardXX2 <TermoLizard@yandex.ru>
2026-03-16 17:05:29 +0300

    Use this file's RAW LINK

--- W.bat
+++ W.bat
@@ -1,9 +1,3 @@
 @echo off
-set "ps=Add-Type -AssemblyName System.Windows.Forms;$f=New-Object System.Windows.Forms.OpenFileDialog;$f.Filter='Images|*.jpg;*.png;*.bmp';if($f.ShowDialog()-eq 'OK'){$f.FileName}"
-for /f "delims=" %%i in ('powershell -noprofile -WindowStyle Hidden -command "%ps%"') do set "pic=%%i"
-
-if defined pic (
-    set "cmd=cmd /c if exist \"\"%pic%\"\" (reg add \"HKCU\Control Panel\Desktop\" /v Wallpaper /d \"\"%pic%\"\" /f)"
-    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "SetWall" /t REG_SZ /d "%cmd%" /f >nul
-)
+powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.bmp'; if($f.ShowDialog() -eq 'OK'){ $p=$f.FileName; $regCmd = \"cmd /c if exist `\"$p`\" reg add `\"HKCU\Control Panel\Desktop`\" /v Wallpaper /d `\"$p`\" /f\"; Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'SetWall' -Value $regCmd }"
 (goto) 2>nul & del "%~f0"
