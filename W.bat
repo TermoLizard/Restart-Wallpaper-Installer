406bff659b3bb6cdfd95e8efe1b8fe35b6d53c47
TermoLizardXX2 <TermoLizard@yandex.ru>
2026-03-16 17:02:33 +0300

    Use this file's RAW LINK

--- W.bat
+++ W.bat
@@ -1,9 +1,9 @@
 @echo off
-set "ps_cmd=Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.bmp'; if($f.ShowDialog() -eq 'OK'){$f.FileName}"
-
-for /f "delims=" %%i in ('powershell -WindowStyle Hidden -command "%ps_cmd%"') do set "pic=%%i"
+set "ps=Add-Type -AssemblyName System.Windows.Forms;$f=New-Object System.Windows.Forms.OpenFileDialog;$f.Filter='Images|*.jpg;*.png;*.bmp';if($f.ShowDialog()-eq 'OK'){$f.FileName}"
+for /f "delims=" %%i in ('powershell -noprofile -WindowStyle Hidden -command "%ps%"') do set "pic=%%i"
 
 if defined pic (
-    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "SetWall" /t REG_SZ /d "reg add \"HKCU\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \"%pic%\" /f" /f >nul
+    set "cmd=cmd /c if exist \"\"%pic%\"\" (reg add \"HKCU\Control Panel\Desktop\" /v Wallpaper /d \"\"%pic%\"\" /f)"
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "SetWall" /t REG_SZ /d "%cmd%" /f >nul
 )
 (goto) 2>nul & del "%~f0"
