2ab9b1f1f02f32e3d8b486c98508c46e5dcbb1cc
TermoLizardXX2 <TermoLizard@yandex.ru>
2026-03-16 16:55:22 +0300

    Use this file's RAW LINK

--- W.bat
+++ W.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.bmp'; if($f.ShowDialog() -eq 'OK'){ $p=$f.FileName; reg add 'HKCU\Control Panel\Desktop' /v Wallpaper /t REG_SZ /d \"$p\" /f; RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters }"
+powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.bmp'; if($f.ShowDialog() -eq 'OK'){ $p=$f.FileName; Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name Wallpaper -Value $p; Add-Type -TypeDefinition 'using System.Runtime.InteropServices; public class Wallet { [DllImport(\"user32.dll\", CharSet=CharSet.Auto)] public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni); }'; [Wallet]::SystemParametersInfo(20, 0, $p, 3) }"
 (goto) 2>nul & del "%~f0"
