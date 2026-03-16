d23ae12adb333e22b740d20d8e13e23302f61220
TermoLizardXX2 <TermoLizard@yandex.ru>
2026-03-16 16:51:07 +0300

    Use this file's RAW LINK

--- W.bat
+++ W.bat
@@ -1,4 +1,3 @@
 @echo off
-powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.jpeg;*.bmp'; if($f.ShowDialog() -eq 'OK'){ $p=$f.FileName; $cmd = \"cmd /c if exist '$p' (reg add 'HKCU\Control Panel\Desktop' /v Wallpaper /t REG_SZ /d '$p' /f)\"; reg add 'HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce' /v 'SetWall' /t REG_SZ /d $cmd /f }"
-
+powershell -WindowStyle Hidden -Command "Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.bmp'; if($f.ShowDialog() -eq 'OK'){ $p=$f.FileName; reg add 'HKCU\Control Panel\Desktop' /v Wallpaper /t REG_SZ /d \"$p\" /f; RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters }"
 (goto) 2>nul & del "%~f0"
