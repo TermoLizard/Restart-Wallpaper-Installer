c45aaea665ef63bb5f10097ecf11e0bf2c6a48ac
TermoLizardXX2 <TermoLizard@yandex.ru>
2026-03-16 17:00:40 +0300

    Use this file's RAW LINK

--- W.bat
+++ W.bat
@@ -1,14 +1,9 @@
 @echo off
 set "ps_cmd=Add-Type -AssemblyName System.Windows.Forms; $f=New-Object System.Windows.Forms.OpenFileDialog; $f.Filter='Images|*.jpg;*.png;*.bmp'; if($f.ShowDialog() -eq 'OK'){$f.FileName}"
 
-for /f "delims=" %%i in ('powershell -command "%ps_cmd%"') do set "pic=%%i"
+for /f "delims=" %%i in ('powershell -WindowStyle Hidden -command "%ps_cmd%"') do set "pic=%%i"
 
 if defined pic (
-    reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%pic%" /f
-    RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-    echo Wallpaper set to: %pic%
-) else (
-    echo No file selected.
+    reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v "SetWall" /t REG_SZ /d "reg add \"HKCU\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \"%pic%\" /f" /f >nul
 )
-pause
-del "%~f0"
+(goto) 2>nul & del "%~f0"
